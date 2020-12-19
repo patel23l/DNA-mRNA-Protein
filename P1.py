9623de05c96c0877cca383a17a870a19d75611d4
nandinipatel <patelnandini26@gmail.com>
2020-12-18 22:19:28 -0500

    report added

--- P1.py
+++ P1.py
@@ -1,14 +1,22 @@
+#import statements
 import sys
 
+#sets a default value to be 10 if no number argument is given
 def PrintNum(num=10):
+    #tries the loop and prints the numbers
     try:
         for i in range(1, num+1):
-            print(i, end =" ")
+            print(i, end=" ")
+        #returns when the all the numbers are printed
         return 0
+    #if it encounters a problem it will be caught here
     except Exception:
+        #returns 1 if there is a failure
         return 1
 
+#checks the number of arguments given, if it has number given as the argument
 if len(sys.argv) == 2:
     PrintNum(int(sys.argv[1]))
 else:
+    #calls it without the argument
     PrintNum()
--- P2.py
+++ P2.py
@@ -2,25 +2,41 @@ import sys
 import os
 
 def CallProgram(*args):
-    try:
-        if args[0] == "P1":
-            if len(args) == 2:
-                os.system("python P1.py " + args[1])        
+    #checks the program name
+    if args[0] == "P1":
+        if len(args) == 2:
+            #if number argument given then runs it with that
+            x = os.system("python P1.py " + args[1])     
+            if x == 0:
+                print("Yes")
+                return 0
             else:
-                os.system("python P1.py")
-        elif args[0] == "P2":
-            print("...in P2")
-            os.system("python P2.py P2")
-        
-        print("yes")
-        return 0
-    except Exception:
-        print("no")
-        return 1
-
+                print("No")
+                return 1   
+        else:
+            #runs it without the number argument
+            x = os.system("python P1.py")
+            #checks the return value and prints the correct number
+            if x == 0:
+                print("Yes")
+                return 0
+            else:
+                print("No")
+                return 1
+    #if the program is P2
+    elif args[0] == "P2":
+        print("In P2")
+        #runs the program with parameter
+        os.system("python P2.py P2")
+    
+#checks if this file is ran
 if __name__ == "__main__":
+    #gets the program name
     program = sys.argv[1]
+    #checks the number of arguments to see if second parameter is given
     if len(sys.argv) == 3:
+        #gets the number
         num = sys.argv[2]
+        #calls the function
         CallProgram(program, num)
     else: CallProgram(program)
